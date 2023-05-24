30cf6689747e34ae9e09fbfcf7374d7e652f5b47
Kulpot <kulpotwahu@gmail.com>
2023-05-25 02:56:04 +0800

    Add project files.

---
+++ IterateBackwardsThroughArray/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IterateBackwardsThroughArray
+{
+    public partial class Form1 : Form
+    {
+        private string[] _sentence = new string[] { "This", "is", "some", "text" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            ListStrings();
+        }
+
+        private void ListStrings()
+        {
+            for(int i = _sentence.Length - 1; i >= 0; i--)
+            {
+                listBox1.Items.Add(_sentence[i]);
+            }
+        }
+    }
+}
